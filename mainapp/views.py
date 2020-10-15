99bb04541e526cfd4ea61fdd522f9f1a2c005e43
unknown <hsjun1996@naver.com>
2020-10-16 03:07:13 +0900

    로그인/회원가입

--- mainapp/views.py
+++ mainapp/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 
 def main(request):
     return render(request,"main.html")
+
+def login(request):
+    return render(request,'login.html')
+
+def signup(request):
+    return render(request, 'signup.html')
