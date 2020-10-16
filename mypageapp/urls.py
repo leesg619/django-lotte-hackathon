f0692e25dcd94c096dfa889c08a5a67ab7a7d89f
YIM-HANMIN <gksals6464@gmail.com>
2020-10-17 02:49:12 +0900

    택배조회, 디테일
    구현완료 모델 수정요망

--- mypageapp/urls.py
+++ mypageapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('post_search.html', views.post_search, name = 'post_search'),
-    path('post_search_detail.html', views.post_search_detail, name='post_search_detail'),
-    path('timeline', views.timeline, name='timeline'),
+    path('post_search/', views.post_search, name = 'post_search'),
+    path('post_search_detail/', views.post_search_detail, name='post_search_detail'),
+    path('timeline/', views.timeline, name='timeline'),
 ]
