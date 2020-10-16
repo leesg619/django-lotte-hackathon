3193bb8458b2e34d0e55329a1ae7552115a6284a
YIM-HANMIN <gksals6464@gmail.com>
2020-10-17 01:35:28 +0900

    2020.10.17 01:34 임한민
    1. 로그인
    2. 회원가입
    3. 로그아웃
    4. base 수정(권한)

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -4,4 +4,5 @@ urlpatterns = [
     path('', views.main, name = 'main'),
     path('login', views.login, name='login'),
     path('signup', views.signup, name='signup'),
+    path('logout', views.logout, name='logout'),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from mypageapp.models import User
+from django.contrib import auth
 
 # Create your views here.
 
@@ -6,7 +8,32 @@ def main(request):
     return render(request,"main.html")
 
 def login(request):
+    if request.method == 'POST':
+        username = request.POST['userId']
+        password = request.POST['userPwd']
+        user = auth.authenticate(request, username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('main')
+        else :
+            return render(request, 'login.html', {'error' : '아이디나 비밀번호가 틀렸습니다.'})
+
     return render(request,'login.html')
 
+
 def signup(request):
-    return render(request, 'signup.html')
+    if request.method == 'POST':
+        if request.POST['password'] == request.POST['repassword']:
+            user = User.objects.create_user(
+                username=request.POST['id'], 
+                password=request.POST['password'],
+                name=request.POST['name'],
+                phone=request.POST['number'],
+                address=request.POST['address'],
+                )
+            return redirect('login')
+    return render(request,'signup.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('login')
