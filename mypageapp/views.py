4d8fa5fcedaded0d5121e5640a30cca57c406155
YIM-HANMIN <gksals6464@gmail.com>
2020-10-17 22:53:52 +0900

    오류수정

--- mypageapp/views.py
+++ mypageapp/views.py
@@ -7,10 +7,10 @@ def post_search(request):
 def post_search_detail(request):
     code = request.POST["code"]
     box = Box.objects.filter(code = code)
-    state = reversed(State.objects.filter(box = box[0]))
     if not box:
         return render(request,'post_search.html',{'error':'존재하지 않는 송장번호입니다.'})
     else :
+        state = reversed(State.objects.filter(box = box[0]))
         return render(request,'post_search_detail.html',{'box':box,'state': state})
     
 def timeline(request):
