cc463c43d718a86b65279a4feddfae14ce3865c0
leesg619 <52132929+leesg619@users.noreply.github.com>
2020-10-17 13:08:06 +0900

    타임라인

--- mypageapp/views.py
+++ mypageapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-
+from django.contrib import auth
+from mainapp.models import *
+from .models import *
 # Create your views here.
 def post_search(request):
     return render(request, 'post_search.html')
@@ -8,4 +10,6 @@ def post_search_detail(request):
     return render(request, 'post_search_detail.html')
     
 def timeline(request):
-    return render(request, 'timeline.html')
+    mybox = Box.objects.filter(receiver = request.user.username)|Box.objects.filter(sender = request.user.username)
+    state = State.objects.all()
+    return render(request, 'timeline.html',{'mybox':mybox,'state':state,})  
