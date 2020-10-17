d90345bfb9a555cd071799eb1c07413852c5e504
unknown <hsjun1996@naver.com>
2020-10-17 15:21:29 +0900

    예약추가/그외수정

--- mypageapp/views.py
+++ mypageapp/views.py
@@ -9,3 +9,15 @@ def post_search_detail(request):
     
 def timeline(request):
     return render(request, 'timeline.html')
+
+def mypage(request):
+    return render(request,'mypage.html')
+
+def post_reservation(request):
+    return render(request,'post_reservation.html')
+
+def post_reserve_look(request):
+    return render(request,'post_reserve_look.html')
+
+def post_reserve_look_detail(request):
+    return render(request,'post_reserve_look_detail.html')
