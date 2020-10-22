b89e66af13ba3e102a6d4d7fcd715fd3033c3611
Bingsuya <qls2540@naver.com>
2020-10-22 22:52:36 +0900

    수정아직덜구현
    url 만빼고 일단 커밋

--- supportapp/urls.py
+++ supportapp/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
    path('custom_voice_new', views.custom_voice_new, name="custom_voice_new"),
    path('custom_voice_detail/<int:pk>', views.custom_voice_detail, name="custom_voice_detail"),
    path('deletevoice/', views.deletevoice, name = 'deletevoice'),
+   path('custom_voice_edit/', views.custom_voice_edit, name = 'custom_voice_edit'),
 ]
--- supportapp/views.py
+++ supportapp/views.py
@@ -72,3 +72,57 @@ def deletevoice(request):
     post = Qna.objects.get(id = post_id)
     post.delete() #타이틀이나 컨텐츠 저장할 필요없이 삭제만 해주면됨
     return redirect( 'custom_voice')
+
+
+def custom_voice_edit(request): #post로 받아오자
+    if request.method == 'POST' and 'pic' in request.FILES:
+        aa = Qna.objects.filter(user = request.user)
+        qna = Qna() 
+        qnacontent = request.POST.get('q_content')
+        qnatitle = request.POST.get('q_title')
+        #qnatype = request.POST.get('q_type')
+        qnaboxcode = request.POST.get('q_boxcode')
+        qnatime = request.POST.get('q_time')
+        qnapublic = request.POST.get('public')
+        pic = request.FILES['pic']
+
+        qna.q_content = qnacontent
+        qna.q_title = qnatitle
+        # qna.qnatype =qnatype
+        qna.q_boxcode =qnaboxcode 
+        qna.q_time =qnatime 
+        qna.public =qnapublic
+        qna.pic = pic
+        qna.user = request.user
+        qna.save()
+        return redirect('custom_voice')
+
+    elif request.method == 'POST':
+        aa = Qna.objects.filter(user = request.user)
+        qna = Qna()
+        qnacontent = request.POST.get('q_content')
+        qnatitle = request.POST.get('q_title')
+        #qnatype = request.POST.get('q_type')
+        qnaboxcode = request.POST.get('q_boxcode')
+        qnatime = request.POST.get('q_time')
+        qnapublic = request.POST.get('public')
+        
+
+        qna.q_content = qnacontent
+        qna.q_title = qnatitle
+        # qna.qnatype =qnatype
+        qna.q_boxcode =qnaboxcode 
+        qna.q_time =qnatime 
+        qna.public =qnapublic
+        
+        qna.user = request.user
+        qna.save()
+        return redirect('custom_voice')
+
+    else :
+        post_id = request.GET['post_id']
+        aa = Qna.objects.get(id = post_id )
+        return render(request, 'custom_voice.html', { 'aa' : aa })
+        
+
+    
